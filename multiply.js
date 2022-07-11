0dd329c9156893d9bd08a285d0235fada3b6592d
aaronlrv <aaronvitharanage@gmail.com>
2022-07-11 17:03:59 +0400

    Write multiply function to pass all test cases

--- multiply.js
+++ multiply.js
@@ -1,3 +1,9 @@
-function multiply() {}
+function multiply(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "Only input numbers!";
+  }
+
+  return a * b;
+}
 
 module.exports = multiply;
