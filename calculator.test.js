146b0ea32679fe0ef80002531945bc51fdaa6782
aaronlrv <aaronvitharanage@gmail.com>
2022-07-11 16:53:19 +0400

    Write first add test

--- calculator.test.js
+++ calculator.test.js
@@ -2,3 +2,7 @@ let add = require("./add");
 let minus = require("./minus");
 let multiply = require("./multiply");
 let divide = require("./divide");
+
+test("add", () => {
+  expect(add(5, 5)).toBe(10);
+});
