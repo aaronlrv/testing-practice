10dbdcaa525c44d20865475d2e5c499073c09401
aaronlrv <aaronvitharanage@gmail.com>
2022-07-11 16:24:19 +0400

    Write second test for capitalize

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ let capitalize = require("./capitalize");
 test("capitalizes string", () => {
   expect(capitalize("hi")).toBe("Hi");
 });
+
+test("Cannot capitalize number", () => {
+  expect(capitalize(8)).toBe("Cannot capitalize anything thats not a string");
+});
