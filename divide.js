d39c0deb9427ea60198950728058c57f7675d2c4
aaronlrv <aaronvitharanage@gmail.com>
2022-07-11 17:05:43 +0400

    Write divide function to pass test

--- divide.js
+++ divide.js
@@ -1,3 +1,13 @@
-function divide() {}
+function divide(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "Only input numbers!";
+  }
+
+  if (a === 0 || b === 0) {
+    return 0;
+  }
+
+  return a / b;
+}
 
 module.exports = divide;
