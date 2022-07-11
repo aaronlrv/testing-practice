62905be3d5e9318490b9c0742a5059c8c431e738
aaronlrv <aaronvitharanage@gmail.com>
2022-07-11 16:39:06 +0400

    Add second test for capitalize

--- reverse.test.js
+++ reverse.test.js
@@ -3,3 +3,7 @@ let reverseString = require("./reversestring");
 test("hello reverse", () => {
   expect(reverseString("hello")).toBe("olleh");
 });
+
+test("reverse with capital", () => {
+  expect(reverseString("Howdy")).toBe("ydwoH");
+});
