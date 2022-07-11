eb50a55abc6e2acca3111e7357911fb26b36a1f0
aaronlrv <aaronvitharanage@gmail.com>
2022-07-11 16:25:05 +0400

    Make capitalize work for second test

--- capitalize.js
+++ capitalize.js
@@ -1,4 +1,8 @@
 function capitalize(string) {
+  if (typeof string !== "string") {
+    return "Cannot capitalize anything thats not a string";
+  }
+
   let capitalLetter = string.charAt(0);
   let word = capitalLetter.toUpperCase() + string.slice(1);
   console.log(word);
